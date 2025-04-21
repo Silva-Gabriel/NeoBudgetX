294cef610822d9bee2d0fca5bf8961b628ecdaaf
Gabriel S S <pottergryff8@gmail.com>
2025-04-21 19:58:19 -0300

    feat: base para o backend

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,4 +1,5 @@
 const passwordField = document.getElementById("password");
+const userField = document.getElementById("user");
 const togglePassword = document.querySelector(".password-toggle-icon");
 
 // Inicializa a senha mascarada
@@ -17,4 +18,39 @@ function showPassword() {
         passwordField.setAttribute("data-mask", "false"); // Custom attribute to indicate unmasked
         passwordField.style.webkitTextSecurity = "none"; // Disable masking
     }
+}
+
+function Login(event) {
+    event.preventDefault();
+    const user = userField.value;
+    const password = passwordField.value;
+
+    console.log("User:", user);
+    console.log("Password:", password);
+
+    fetch("http://localhost:5000/v1/auth", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ user, password }),
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            if (data.valid) {
+                alert("Login successful!");
+                // Redirect or perform other actions
+            } else {
+                alert("Invalid user or password.");
+            }
+        })
+        .catch((error) => {
+            console.error("There was a problem with the fetch operation:", error);
+            alert("An error occurred. Please try again later.");
+        });
 }
